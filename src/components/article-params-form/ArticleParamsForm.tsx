7cb5f4e826e667e7f4fa0213fb083800377cde19
Sergey S. <karpseledka@gmail.com>
2024-11-09 16:22:31 +0700

    try #1.1

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -1,14 +1,141 @@
 import { ArrowButton } from 'src/ui/arrow-button';
+import { Text } from 'src/ui/text';
 import { Button } from 'src/ui/button';
 
-import styles from './ArticleParamsForm.module.scss';
+import {
+	ArticleStateType,
+	backgroundColors,
+	contentWidthArr,
+	defaultArticleState,
+	fontColors,
+	fontFamilyOptions,
+	fontSizeOptions,
+} from 'src/constants/articleProps';
 
-export const ArticleParamsForm = () => {
+import styles from './ArticleParamsForm.module.scss';
+import { Select } from 'src/ui/select';
+import { FormEvent, useState, useRef } from 'react';
+import { useOutsideClickClose } from 'src/ui/select/hooks/useOutsideClickClose';
+
+import { RadioGroup } from 'src/ui/radio-group';
+import { Separator } from 'src/ui/separator';
+import clsx from 'clsx';
+
+type ArticleParamsProps = {
+	articleState: ArticleStateType;
+	setArticleState: (param: ArticleStateType) => void;
+};
+
+export const ArticleParamsForm = ({
+	articleState,
+	setArticleState,
+}: ArticleParamsProps) => {
+	const [formState, setFormState] = useState({
+		fontFamilyOption: articleState.fontFamilyOption,
+		fontColor: articleState.fontColor,
+		backgroundColor: articleState.backgroundColor,
+		contentWidth: articleState.contentWidth,
+		fontSizeOption: articleState.fontSizeOption,
+	});
+	const [isOpen, setIsOpen] = useState<boolean>(false);
+
+	const rootRef = useRef<HTMLDivElement | null>(null);
+	const formRef = useRef<HTMLFormElement>(null);
+
+	useOutsideClickClose({
+		isOpen,
+		rootRef,
+		onClose: () => setIsOpen(false),
+		onChange: setIsOpen,
+	});
+
+	const formResetHandler = () => {
+		setFormState(() => ({
+			...defaultArticleState,
+		}));
+
+		setArticleState(defaultArticleState);
+	};
+
+	const formSubmitHandler = (evt: FormEvent) => {
+		evt.preventDefault();
+
+		setArticleState({
+			...formState,
+		});
+	};
+
 	return (
 		<>
-			<ArrowButton isOpen={false} onClick={() => {}} />
-			<aside className={styles.container}>
-				<form className={styles.form}>
+			<ArrowButton isOpen={isOpen} onClick={setIsOpen} />
+			<aside
+				className={clsx(styles.container, isOpen && styles.container_open)}
+				ref={rootRef}>
+				<form
+					className={styles.form}
+					onReset={formResetHandler}
+					onSubmit={formSubmitHandler}
+					ref={formRef}>
+					<Text as={'h2'} size={31} weight={800} uppercase>
+						Задайте параметры
+					</Text>
+					<Select
+						title='Шрифт'
+						selected={formState.fontFamilyOption}
+						options={fontFamilyOptions}
+						onChange={(selectedOptions) =>
+							setFormState((prevState) => ({
+								...prevState,
+								fontFamilyOption: selectedOptions,
+							}))
+						}
+					/>
+					<RadioGroup
+						title='Размер шрифта'
+						name='Размер шрифта'
+						selected={formState.fontSizeOption}
+						options={fontSizeOptions}
+						onChange={(selectedOption) =>
+							setFormState((prevState) => ({
+								...prevState,
+								fontSizeOption: selectedOption,
+							}))
+						}
+					/>
+					<Select
+						title='Цвет шрифта'
+						options={fontColors}
+						selected={formState.fontColor}
+						onChange={(selectedOption) =>
+							setFormState((prevState) => ({
+								...prevState,
+								fontColor: selectedOption,
+							}))
+						}
+					/>
+					<Separator />
+					<Select
+						title='Цвет фона'
+						options={backgroundColors}
+						selected={formState.backgroundColor}
+						onChange={(selectedOption) =>
+							setFormState((prevState) => ({
+								...prevState,
+								backgroundColor: selectedOption,
+							}))
+						}
+					/>
+					<Select
+						title='Ширина контента'
+						options={contentWidthArr}
+						selected={formState.contentWidth}
+						onChange={(selectedOption) =>
+							setFormState((prevState) => ({
+								...prevState,
+								contentWidth: selectedOption,
+							}))
+						}
+					/>
 					<div className={styles.bottomContainer}>
 						<Button title='Сбросить' htmlType='reset' type='clear' />
 						<Button title='Применить' htmlType='submit' type='apply' />
